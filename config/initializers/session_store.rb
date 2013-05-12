f0d0df8083fcc189c43729c05116f89cd667df27
henning mueller <henning@orgizm.net>
2013-05-12 23:47:43 +0200

    UTF-8 grinches...

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Moeil::Application.config.session_store :cookie_store, key: '_møil_session'
+Moeil::Application.config.session_store :cookie_store, key: '_moeil_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
