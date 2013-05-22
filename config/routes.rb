bcef1a82d736e237c2b477c34f0498e0d2d3b3d8
henning mueller <henning@orgizm.net>
2013-05-23 01:58:09 +0200

    A quick landing page, for the default route not to point to login.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Moeil::Application.routes.draw do
   devise_for :mailboxes, path: '',
     path_names: {sign_in: 'login', sign_out: 'logout' }
 
+  resource :home,     only: [:show]
   resource :password, only: [:show, :update]
 
   resources :domains, only: [:index] do
@@ -9,6 +10,6 @@ Moeil::Application.routes.draw do
   end
 
   devise_scope :mailbox do
-    root to: 'devise/sessions#new'
+    root to: 'home#show'
   end
 end
