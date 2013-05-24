c7b9f4d5ef414f0da52a63d51b593b7995c264bc
henning mueller <henning@orgizm.net>
2013-05-24 18:02:44 +0200

    Changed database config for SQLite.

--- config/deploy/db.rb
+++ config/deploy/db.rb
@@ -1,8 +1,14 @@
 before 'deploy:assets:precompile', 'db:use_sqlite'
+after 'db:use_sqlite', 'db:migrate'
  
 namespace :db do
   desc 'Use SQLite database for testing in production.'
   task :use_sqlite do
-    run "cd #{latest_release}/config && cp database.yml.sqlite database.yml"
+    run "cd #{latest_release}/config && cp database.yml.sqlite3 database.yml"
+  end
+
+  desc 'Migrate database.'
+  task :migrate do
+    run "cd #{latest_release} && RAILS_ENV=production rake db:migrate"
   end
 end
