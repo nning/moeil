1b655393bc37ff366b76be4ea38f471e67ed6bb3
henning mueller <henning@orgizm.net>
2013-05-25 03:46:50 +0200

    Removed SQLite from deployment configuration.

--- config/deploy/db.rb
+++ config/deploy/db.rb
@@ -2,11 +2,6 @@ before 'deploy:assets:precompile', 'db:use_pgsql'
 after 'db:use_pgsql', 'db:migrate'
  
 namespace :db do
-  desc 'Use SQLite database in production.'
-  task :use_sqlite do
-    run "cd #{latest_release}/config && cp database.yml.sqlite3 database.yml"
-  end
-
   desc 'Use PostgreSQL database in production.'
   task :use_pgsql do
     run "cd #{latest_release}/config && cp database.yml.pgsql database.yml"
