b4aa1b1b021a756aa51ae58f16a545fec102c3f8
henning mueller <henning@orgizm.net>
2013-06-20 22:25:21 +0200

    Handle display of deleted objects in change index.

--- app/helpers/version_helper.rb
+++ app/helpers/version_helper.rb
@@ -1,19 +1,16 @@
 module VersionHelper
   
-  def link_to_object(clazz, id)
-    object = Object.const_get(clazz).find(id)
+  def link_to_object(version)
+    clazz = version.item_type
+    id    = version.item_id
 
-    case clazz
-      when 'Domain'
-        text = object.name
-        url = edit_admin_domain_path(object.id)
-      when 'Alias'
-        text = object.email
-        url = edit_admin_domain_alias_path(object.domain.id, object.id)
-      when 'Mailbox'
-        text = object.email
-        url = edit_admin_domain_mailbox_path(object.domain.id, object.id)
+    begin
+      object = Object.const_get(clazz).find(id)
+    rescue ActiveRecord::RecordNotFound
+      return alternative_name(version)
     end
+
+    text, url = get_text_and_url(clazz, object)
 
     html = ''
     html << link_to(text, url)
@@ -31,11 +28,46 @@ module VersionHelper
   end
 
   def summarize_changes(changes)
+    return unless changes
+
     html = ''
     YAML.load(changes).each do |k, v|
       html << content_tag(:p, "#{k}: #{v}")
     end
     html.html_safe
+  end
+
+private
+
+  def alternative_name(version)
+    if version.object
+      hash   = YAML.load(version.object)
+      domain = Domain.find(hash['domain_id']).name
+
+      return [hash['username'], domain].join('@')
+    end
+
+    hash     = YAML.load(version.object_changes)
+    username = hash['username'].last 
+    domain   = Domain.find(hash['domain_id'].last).name
+
+    return [username, domain].join('@')
+  end
+
+  def get_text_and_url(clazz, object)
+    case clazz
+      when 'Domain'
+        text = object.name
+        url = edit_admin_domain_path(object.id)
+      when 'Alias'
+        text = object.email
+        url = edit_admin_domain_alias_path(object.domain.id, object.id)
+      when 'Mailbox'
+        text = object.email
+        url = edit_admin_domain_mailbox_path(object.domain.id, object.id)
+    end
+
+    [text, url]
   end
 
 end
