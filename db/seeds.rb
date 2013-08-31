f8baf9b68b10d90401b72e9d3a7d9af6d66dcf65
henning mueller <henning@orgizm.net>
2013-09-01 01:02:29 +0200

    Checked out faster finishing seeds from permissions branch.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,31 @@
 require 'factory_girl_rails'
 require 'faker'
 
+def rand5; rand(5) + 1; end
+
 ActiveRecord::Base.logger = Logger.new($stdout) unless ENV['DEBUG'].blank?
 
-d = Domain.create name: 'example.org'
-d.mailboxes.create username: 'jane.doe', password: 'foobar', admin: true
+d, p  = Domain.create(name: 'example.org'), 'foobar'
+alice = d.mailboxes.create username: 'alice', password: p, admin: true
+bob   = d.mailboxes.create username: 'bob',   password: p
+carol = d.mailboxes.create username: 'carol', password: p
+dan   = d.mailboxes.create username: 'dan',   password: p
 
-puts 'Created mailbox jane.doe@example.org (password "foobar") with admin rights.'
+puts <<O
+Created mailboxes
+  alice@example.org with admin rights,
+  bob@example.org as owner of some domains,
+  carol@example.org as editor of some domains,
+  dan@example.org without special rights
+  (all with password "foobar")
+O
 
-(rand(20) + 1).times do
+rand5.times do
   d = FactoryGirl.create :domain
 
   begin
-    (rand(50) + 1).times { FactoryGirl.create :mailbox, domain: d }
-    (rand(20) + 1).times { FactoryGirl.create :alias, domain: d }
+    rand5.times { FactoryGirl.create :mailbox, domain: d }
+    rand5.times { FactoryGirl.create :alias, domain: d }
   rescue
   end
 end
