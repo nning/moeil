2c98d1211dbcb22ceb04f7aabd2463e5b40590c3
henning mueller <henning@orgizm.net>
2013-09-17 00:26:33 +0200

    Link to catch-all goto alias or mailbox if existing.

--- app/helpers/alias_and_mailbox_helper.rb
+++ app/helpers/alias_and_mailbox_helper.rb
@@ -4,4 +4,20 @@ module AliasAndMailboxHelper
     Domain.where(id: params[:domain_id]).first
   end
 
+  def link_to_alias_or_mailbox(email)
+    return nil if email.nil?
+
+    username, domain = email.split('@')
+    domain = Domain.where(name: domain).first
+
+    return email if domain.nil?
+
+    o   = domain.aliases.where(username: username).first
+    o ||= domain.mailboxes.where(username: username).first
+
+    return email if o.nil?
+
+    link_to email, [:edit, :admin, o.domain, o]
+  end
+
 end
