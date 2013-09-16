d161fa0cdc0460f3f2712b1973f3cbb1e810ba13
henning mueller <henning@orgizm.net>
2013-09-17 00:07:11 +0200

    Forgot to add database migration to commit.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130916154005) do
+ActiveRecord::Schema.define(:version => 20130916220251) do
 
   create_table "aliases", :force => true do |t|
     t.string   "username"
