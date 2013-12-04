4843d64be79b35b31fdebfc316ff793b7a488799
henning mueller <henning@orgizm.net>
2013-12-04 16:49:01 +0100

    Updated rails to 3.2.16.

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,7 @@ module Moeil
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.i18n.enforce_available_locales = true
   end
 end
