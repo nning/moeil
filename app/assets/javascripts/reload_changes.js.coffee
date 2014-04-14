7dbb9b79a6564df1a4a53d795fd20a21f0b01ac6
henning mueller <henning@orgizm.net>
2014-04-14 15:09:56 +0200

    Better updating of versions#index.

--- app/assets/javascripts/reload_changes.js.coffee
+++
@@ -1,5 +0,0 @@
-$ ->
-  if $('h1').html() == 'Changes'
-    setTimeout(->
-      location.reload()
-    , 120 * 1000)
--- app/controllers/admin/versions_controller.rb
+++ app/controllers/admin/versions_controller.rb
@@ -20,4 +20,11 @@ class Admin::VersionsController < AdminController
       redirect_to [:admin, :versions], flash: { error: "Impossible to revert #{@version.event}." }
     end
   end
+
+  # Return versions since an id rendered as HTML.
+  def updates
+    since = params[:since].to_i || 0
+    @versions = Version.where('id > ?', since).order('created_at desc')
+    render @versions
+  end
 end
